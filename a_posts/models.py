005ae1f6a803d195e9fad355b4284ce1741d7eae
Sunil  Kunkekar <sunilkunkekar@percipient.in>
2025-02-14 19:11:51 +0530

    define the model class and views class for Image post

--- a_posts/models.py
+++ a_posts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+class POST(models.Model):
+    title = models.CharField(max_length=100)
+    image = models.URLField(max_length=100)
+    body  = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    id  = models.CharField(max_length=100,default=uuid.uuid4, unique=True,primary_key=True,editable=False)
+    
+    def __str__(self):
+        return self.title
--- a_posts/views.py
+++ a_posts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-
+from a_posts.models import *
 # Create your views here.
 def home(request):
-    print("Hello")
-    return render(request,'a_posts/home.html')
+    posts = POST.objects.all()
+    return render(request,'a_posts/home.html',{'posts':posts})
