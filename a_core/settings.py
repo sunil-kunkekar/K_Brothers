f886231193a65642c33e46051270fbd017f7911b
Sunil  Kunkekar <sunilkunkekar@percipient.in>
2025-02-26 23:21:35 +0530

    Implement the Email configurations in settings file for Email for Verification

--- a_core/settings.py
+++ a_core/settings.py
@@ -148,3 +148,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = '/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+ACCOUNT_AUTHENTICATION_METHOD = 'email'
+ACCOUNT_EMAIL_REQUIRED = True
