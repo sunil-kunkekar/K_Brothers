c2fec92c616bd07bad4b3d30f23f8271dbc25184
Sunil  Kunkekar <sunilkunkekar@percipient.in>
2025-02-26 11:57:55 +0530

    Implement the Django-Allauth templates styling

--- a_core/settings.py
+++ a_core/settings.py
@@ -146,3 +146,5 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/'
