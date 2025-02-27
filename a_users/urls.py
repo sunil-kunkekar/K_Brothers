2ba30444dd024779ea4105baac831f810184b6d4
Sunil  Kunkekar <sunilkunkekar@percipient.in>
2025-02-27 23:34:14 +0530

    Implement the Profile edit page from both side

--- a_users/urls.py
+++ a_users/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from a_users.views import *
-
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('profile/', profile_view, name='profile'),
+    path('profile/edit/',profile_edit_view, name='profile-edit'),
     
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
