7f50a8dd7dceeb776a0e0210a2c07181b8c0e41f
Sunil  Kunkekar <sunilkunkekar@percipient.in>
2025-03-14 20:31:34 +0530

    update the post template file

--- a_posts/urls.py
+++ a_posts/urls.py
@@ -13,9 +13,11 @@ urlpatterns = [
     
     path('commentsent/<pk>/', comment_sent, name='comment-sent'),
     path('comment/delete/<pk>/',comment_delete_view, name='comment-delete'),
+    
     path('reply-sent/<pk>/',reply_sent, name='reply-sent'),
     path('reply/delete/<pk>/',reply_delete_view, name='reply-delete'),
     path('post/like/<pk>/', like_post, name="like-post"), 
+    path('post/<pk>/', post_detail_page_view, name="post"), 
     
     path('comment/like/<pk>/', like_comment, name="like-comment"), 
     path('reply/like/<pk>/', like_reply, name="like-reply"),
