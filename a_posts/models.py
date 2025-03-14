5da0d0fd464eaabffb15ea4b8bbde25f8c584e6b
Sunil  Kunkekar <sunilkunkekar@percipient.in>
2025-03-14 11:49:05 +0530

    Adding the Likepost class model into post section and add the count of the likes in post template file

--- a_posts/models.py
+++ a_posts/models.py
@@ -11,6 +11,7 @@ class POST(models.Model):
     author = models.ForeignKey(User,on_delete=models.SET_NULL,null=True,related_name='posts')
     tags = models.ManyToManyField('Tag')
     body  = models.TextField()
+    likes = models.ManyToManyField(User, related_name="likedposts", through="LikedPost")
     created_at = models.DateTimeField(auto_now_add=True)
     id  = models.CharField(max_length=100,default=uuid.uuid4, unique=True,primary_key=True,editable=False)
     
@@ -18,7 +19,14 @@ class POST(models.Model):
         return self.title
     class Meta:
         ordering = ['-created_at']
-        
+
+class LikedPost(models.Model):
+    post = models.ForeignKey(POST, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f'{self.user.username} : {self.post.title}'
 
 class Tag(models.Model):
     name = models.CharField(max_length=20)
